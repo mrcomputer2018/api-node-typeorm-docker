08c027da6e708b2ea64a300904b515ba144a0973
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-06 15:51:14 -0300

    Add error handling and database retrieval to class route

--- src/routes/class.routes.ts
+++ src/routes/class.routes.ts
@@ -1,11 +1,19 @@
 // Responsavel por todas as rotas da classe
+import app from '../app';
 import { AppDataSource } from './../database/data-source';
 import { Router } from 'express';
 
 const classRouter = Router();
 
-classRouter.get('/', (req, res) => {
-    res.json({ message: 'Hello, World!' });
+classRouter.get('/', async(req, res) => {
+    try {
+        const response = await AppDataSource.getRepository('Class').find();
+        res.status(200).json(response);
+    }
+    catch (error) {
+        console.log('Erro ao buscar as classes: ', error.message);
+        res.status(500).json({ message: 'Erro ao buscar as classes' });
+    }
 });
 
 classRouter.get('/:id', (req, res) => {
