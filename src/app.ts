8b726bd80213b79f190df3ea267db9eeb5914663
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-06 15:34:15 -0300

    Refactor route structure by moving routes to index file and removing old routes file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'express-async-errors';
 import express from 'express';
 import cors from 'cors';
-import  routes  from './routes/routes';
+import  routes  from './routes';
 
 const app = express();
 
