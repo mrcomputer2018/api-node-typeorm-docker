ede9f5f7bb3e68788391ea35f43181ebad452486
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-11 05:46:30 -0300

    Enhance Lesson model with created_at and updated_at timestamps, and enforce non-null constraints on name and description fields

--- src/models/Lesson.ts
+++ src/models/Lesson.ts
@@ -1,13 +1,19 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
 @Entity('lesson')
 export class Lesson {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column('varchar', { length: 100 })
+    @Column('varchar', { length: 100, nullable: false })
     name: string;
+
+    @Column('varchar', { length: 255, nullable: false })
     description: string;
+
+    @CreateDateColumn()
     created_at: Date;
+
+    @UpdateDateColumn()
     updated_at: Date;
 }
